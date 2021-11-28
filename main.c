f65f6d9d4c806e7ebaabf4f38ec3a350e049d232
James Tan <james_tan97@outlook.com>
2021-11-27 22:41:24 -0500

    Implement test suite. Fix minor bugs regarding max string size. Add more validations for inputs.

--- main.c
+++ main.c
@@ -28,6 +28,8 @@ typedef enum {
 typedef enum {
   PREPARE_SUCCESS,
   PREPARE_SYNTAX_ERROR,
+  PREPARE_STRING_TOO_LONG,
+  PREPARE_NEGATIVE_ID,
   PREPARE_UNRECOGNIZED_STATEMENT,
 } PrepareResult;
 
@@ -37,8 +39,9 @@ typedef enum {
 // Temporarily hardcoding the format of a row
 typedef struct {
   uint32_t id;
-  char username[COLUMN_USERNAME_SIZE];
-  char email[COLUMN_EMAIL_SIZE];
+  // +1 for space for null character
+  char username[COLUMN_USERNAME_SIZE + 1]; 
+  char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
@@ -107,15 +110,39 @@ void* row_slot(Table *table, uint32_t row_num) {
   return page + byte_offset;
 }
 
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
+  statement->type = STATEMENT_INSERT;
+
+  char* keyword = strtok(input_buffer->buffer, " ");
+  char* id_string = strtok(NULL, " ");
+  char* username = strtok(NULL, " ");
+  char* email = strtok(NULL, " ");
+
+  if (id_string == NULL || username == NULL || email == NULL) {
+    return PREPARE_SYNTAX_ERROR;
+  }
+
+  int id = atoi(id_string);
+  if (id < 0) {
+    return PREPARE_NEGATIVE_ID;
+  }
+
+  if (strlen(username) > COLUMN_USERNAME_SIZE) {
+    return PREPARE_STRING_TOO_LONG;
+  }
+
+  statement->row_to_insert.id = id;
+  strcpy(statement->row_to_insert.username, username);
+  strcpy(statement->row_to_insert.email, email);
+
+  return PREPARE_SUCCESS;
+}
+
 // This is the front-end of our database, where we convert 
 // user input into our internal representation of a Statement
 PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement) {
   if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-    statement->type = STATEMENT_INSERT;
-    int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
-    if (args_assigned < 3) 
-      return PREPARE_SYNTAX_ERROR;
-    return PREPARE_SUCCESS;
+    return prepare_insert(input_buffer, statement);
   }
 
   if (strcmp(input_buffer->buffer, "select") == 0) {
@@ -237,6 +264,12 @@ int main(int argc, char* argv[]) {
         continue;
       case PREPARE_SYNTAX_ERROR:
         printf("Syntax error. Could not parse statement.\n");
+        continue;
+      case PREPARE_STRING_TOO_LONG:
+        printf("String is too long.\n");
+        continue;
+      case PREPARE_NEGATIVE_ID:
+        printf("ID must be positive.\n");
         continue;
     }
 
