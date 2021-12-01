1ee3df0e29a4c9917166203967f906a570b16a12
James Tan <james_tan97@outlook.com>
2021-12-01 14:37:34 -0500

    Implement recursive searching of the b-tree

--- cursor.c
+++ cursor.c
@@ -50,6 +50,35 @@ static Cursor* leaf_node_find(Table *table, uint32_t page_num, uint32_t key) {
   return cursor;
 }
 
+static Cursor* internal_node_find(Table* table, uint32_t page_num, uint32_t key) {
+  void* node = get_page(table->pager, page_num);
+  uint32_t num_keys = *internal_node_num_keys(node);
+
+  // Binary search to find the right child to search
+  uint32_t min_index = 0;
+  uint32_t max_index = num_keys; // 1 more child than num of keys
+
+  while (min_index != max_index) {
+    uint32_t index = (min_index + max_index) / 2;
+    uint32_t key_to_right = *internal_node_key(node, index);
+
+    if (key_to_right >= key) {
+      max_index = index;
+    } else {
+      min_index = index + 1;
+    }
+  }
+
+  uint32_t child_num = *internal_node_child(node, min_index);
+  void* child = get_page(table->pager, child_num);
+  switch (get_node_type(child)) {
+    case NODE_LEAF:
+      return leaf_node_find(table, child_num, key);
+    case NODE_INTERNAL:
+      return internal_node_find(table, child_num, key);
+  }
+}
+
 Cursor* table_find(Table* table, uint32_t key) {
   uint32_t root_page_num = table->root_page_num;
   void* root_node = get_page(table->pager, root_page_num);
@@ -57,8 +86,7 @@ Cursor* table_find(Table* table, uint32_t key) {
   if (get_node_type(root_node) == NODE_LEAF) {
     return leaf_node_find(table, root_page_num,  key);
   } else {
-    printf("TODO: Implement searching an internal node.\n");
-    exit(EXIT_FAILURE);
+    return internal_node_find(table, root_page_num, key);
   }
 }
 
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -11,7 +11,11 @@ describe 'database' do
     raw_output = nil
     IO.popen("./db test.db", "r+") do |pipe|
       commands.each do |command|
-        pipe.puts command
+        begin
+          pipe.puts command
+        rescue Errno::EPIPE
+          break
+        end
       end
 
       pipe.close_write
@@ -42,7 +46,10 @@ describe 'database' do
     end
     script << ".exit"
     result = run_script(script)
-    expect(result[-2]).to eq('db > Error: Table full.')
+    expect(result.last(2)).to match_array([
+      "db > Executed.",
+      "db > TODO: Need to implement updating parent after split."
+    ])
   end
 
   it 'allows inserting strings that are the maximum length' do
@@ -197,7 +204,8 @@ describe 'database' do
       "    - 12",
       "    - 13",
       "    - 14",
-      "db > TODO: Implement searching an internal node.",
+      "db > Executed.",
+      "db > ",
     ])
   end
 end
